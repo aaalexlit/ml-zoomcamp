7bdd5087495a668bd54b87f70cec18e4d73eb29d
Alex Litvinov <aaalex.lit@gmail.com>
2023-06-20 06:10:27 +0200

    small refactor

--- web-service/predict.py
+++ web-service/predict.py
@@ -6,10 +6,10 @@ with open('lin_reg.bin', 'rb') as f_in:
 
 
 def prepare_features(ride):
-    features = {}
-    features['PU_DO'] = f"{ride['PULocationID']}_{ride['DOLocationID']}"
-    features['trip_distance'] = ride['trip_distance']
-    return features
+    return {
+        'PU_DO': f"{ride['PULocationID']}_{ride['DOLocationID']}",
+        'trip_distance': ride['trip_distance'],
+    }
 
 
 def predict(features):
