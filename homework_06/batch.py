bafa1a2cc6c8dcf52ff1e5b8405b06230bf4f4e2
Alex Litvinov <aaalex.lit@gmail.com>
2023-07-15 22:05:11 +0200

    make things work with s3

--- homework_06/batch.py
+++ homework_06/batch.py
@@ -2,12 +2,27 @@
 # coding: utf-8
 
 import sys
+import os
 import pickle
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_storage_options(filename):
+    if filename.startswith("s3"):
+        if s3_endpoint_url := os.getenv('S3_ENDPOINT_URL'):
+            return {
+                'client_kwargs': {
+                    'endpoint_url': s3_endpoint_url
+                }
+            }
 
 
 def read_data(filename):
-    return pd.read_parquet(filename)
+    options = get_storage_options(filename)
+    return pd.read_parquet(filename, storage_options=options)
 
 
 def prepare_data(df, categorical):
@@ -22,9 +37,21 @@ def prepare_data(df, categorical):
     return df
 
 
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
+
+
 def main(year: int, month: int):
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_file = get_input_path(year, month)
+    output_file = get_output_path(year, month)
 
     with open('model.bin', 'rb') as f_in:
         dv, lr = pickle.load(f_in)
@@ -41,11 +68,23 @@ def main(year: int, month: int):
 
     print('predicted mean duration:', y_pred.mean())
 
+    save_data(df, y_pred, output_file)
+
+
+def save_data(df: pd.DataFrame, y_pred, output_file: str) -> None:
     df_result = pd.DataFrame()
     df_result['ride_id'] = df['ride_id']
     df_result['predicted_duration'] = y_pred
 
-    df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    options = get_storage_options(output_file)
+
+    df_result.to_parquet(
+        output_file,
+        engine='pyarrow',
+        compression=None,
+        index=False,
+        storage_options=options
+    )
 
 
 if __name__ == '__main__':
--- homework_06/homework_06.md
+++ homework_06/homework_06.md
@@ -14,3 +14,51 @@ pipenv run python batch.py 2022 2
 ```shell
 pipenv install --dev pytest
 ```
+
+Spin up just localstack service in docker compose
+
+```shell
+docker compose up localstack -d
+```
+
+create an s3 bucket there
+```shell
+aws --endpoint-url=http://localhost:4566/ s3 mb s3://nyc-duration
+```
+
+check that the bucket has been created
+```shell
+aws --endpoint-url=http://localhost:4566/ s3 ls
+```
+
+---
+### Test the version that reads from s3 bucket
+
+1. Download the file
+
+    ```shell
+    curl --output yellow_tripdata_2022-02.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet
+    ```
+
+1. Copy it into the running localstack
+
+    ```shell
+    aws --endpoint-url=http://localhost:4566/ s3 cp yellow_tripdata_2022-02.parquet s3://nyc-duration/in/2022-02.parquet
+    ```
+
+---
+
+## Important! 
+
+All the environment variables need to be set through the [.env](.env) file
+
+### Example:
+
+```shell
+export S3_ENDPOINT_URL="http://localhost:4566/"
+export S3_BUCKET_NAME="nyc-duration"
+export INPUT_FILE_PATTERN="s3://${S3_BUCKET_NAME}/in/{year:04d}-{month:02d}.parquet"
+export OUTPUT_FILE_PATTERN="s3://${S3_BUCKET_NAME}/out/{year:04d}-{month:02d}.parquet"
+```
+
+---
