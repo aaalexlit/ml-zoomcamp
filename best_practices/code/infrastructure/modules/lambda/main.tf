9f48cc4dc9deda22ba36810652817b1336300254
Alex Litvinov <aaalex.lit@gmail.com>
2023-07-28 13:42:43 +0200

    set platform for lambda explicitly in docker compose and remove the architecture parameter from terraform

--- best_practices/code/infrastructure/modules/lambda/main.tf
+++ best_practices/code/infrastructure/modules/lambda/main.tf
@@ -4,7 +4,6 @@ resource "aws_lambda_function" "kinesis_lambda" {
   # which would be anyway updated regularly via a CI/CD pipeline
   image_uri     = var.image_uri # required-argument
   package_type  = "Image"
-  architectures = ["arm64"]
   role          = aws_iam_role.iam_lambda.arn
   tracing_config {
     mode = "Active"
